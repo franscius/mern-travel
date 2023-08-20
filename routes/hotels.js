d413eaaf11493c76acdb6abccc8aab6b2d9b65a1
Franscius <53150222+franscius@users.noreply.github.com>
2023-08-20 20:29:31 +0700

    Add files via upload

---
+++ routes/hotels.js
@@ -0,0 +1,37 @@
+import express from "express";
+import Hotel from "../models/Hotel.js";
+import { 
+    countByCity,
+    countByType,
+    createHotel, 
+    deleteHotel, 
+    getHotel, 
+    getHotelRooms, 
+    getHotels, 
+    updateHotel 
+} from "../controllers/hotel.js";
+import {verifyAdmin} from "../utils/verifyToken.js"
+import { updateRoomAvailability } from "../controllers/room.js";
+
+const router = express.Router();
+
+//CREATE
+router.post("/", verifyAdmin ,createHotel);
+
+//UPDATE
+router.put("/:id", verifyAdmin, updateHotel);
+router.put("/availability/:id", updateRoomAvailability);
+
+//DELETE
+router.delete("/:id", verifyAdmin, deleteHotel);
+
+//GET
+router.get("/find/:id", getHotel);
+
+//GET ALL
+router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
+router.get("/room/:id", getHotelRooms);
+
+export default router
