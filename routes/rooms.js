4329636db15f6dc73d2b25d0d34eb555100ea71d
Franscius <53150222+franscius@users.noreply.github.com>
2023-08-25 12:14:49 +0700

    Update rooms.js

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,18 +1,17 @@
 import express from "express";
-import { verifyAdmin } from "../utils/verifyToken.js";
 import { createRoom, deleteRoom, getRoom, getRooms, getselectedRooms, updateRoom, updateRoomAvailability,} from "../controllers/room.js";
 
 const router = express.Router();
 
 //CREATE
-router.post("/:hotelid", verifyAdmin, createRoom);
+router.post("/:hotelid", createRoom);
 
 //UPDATE
 router.put("/availability/:id", updateRoomAvailability);
-router.put("/:id", verifyAdmin, updateRoom);
+router.put("/:id", updateRoom);
 
 //DELETE
-router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+router.delete("/:id/:hotelid", deleteRoom);
 //GET
 
 router.get("/:id", getRoom);
